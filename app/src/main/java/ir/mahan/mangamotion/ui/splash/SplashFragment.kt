36daad58b2ca9951f0ea3cbb785d23e39925553b
Mahan Ziyari <55763393+MahanZiyari@users.noreply.github.com>
2025-04-11 21:37:34 +0330

    feat: design splash screen - add animation to splash screen - add SplashFragment to  navigation

---
+++ app/src/main/java/ir/mahan/mangamotion/ui/splash/SplashFragment.kt
@@ -0,0 +1,43 @@
+package ir.mahan.mangamotion.ui.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import ir.mahan.mangamotion.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SplashFragment : Fragment() {
+
+    // Binding
+    private var _binding: FragmentSplashBinding? = null
+    val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            delay(2500)
+
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
